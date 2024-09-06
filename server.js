06c9644ee25935aec1eb521ded837955bc33757b
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-09-06 15:13:08 +0530

    some

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import { connectDB } from "./config/db.js";
 import productRouter from "./routes/productRoute.js";
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config'
-import Razorpay from "razorpay"
-import crypto from "crypto"
 
 
 //app config
@@ -32,42 +30,3 @@ app.use("/api/user",userRouter)
  app.listen(port,() =>{
     console.log(`Server Started on http://localhost:${port}`)
  })
-
-
-
-
-// checkout api
-app.post("/checkout",async(req,res)=>{
-
-    const options ={
-        amount:Number(req.body.amount*100),
-        currency:"INR",
-    };
-    const order = await instance.orders.create(options);
-    console.log(order);
-    res.status(200).json({
-        success:true,order
-    })
-
-})
-
-// payemnt verification
-app.post("/paymentverification",async(req,res)=>{
-   const {razorpay_order_id,razorpay_payment_id,razorpay_signature}=req.body;
-   const body = razorpay_order_id + "|" +razorpay_payment_id;
-   const expectedsgnature =crypto.createHmac('sha256',process.env.RAZORPAY_SECRET).update(body.toString()).digest('hex')
-   const isauth = expectedsgnature === razorpay_signature;
-   if(isauth){
-    await Payment.create({
-        razorpay_order_id,razorpay_payment_id,razorpay_signature
-    })
-    res.redirect(`http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`)
-   }
-   else{
-    res.status(400).json({success:false});
-   }
-})
-
-app.get("/api/getkey",(req,res)=>{
-    return res.status(200).json({key:process.env.RAZORPAY_KEY_ID})
-})
