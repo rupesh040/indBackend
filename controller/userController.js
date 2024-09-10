e4d24d2a66554ffc36c205f5157ce4c133cf719c
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-09-10 09:03:11 +0530

    add

--- controller/userController.js
+++ controller/userController.js
@@ -17,7 +17,7 @@ try {
     const isMatch = await bcrypt.compare(password,user.password);
     if (!isMatch) {
         return res.json({success:false,message:"Invalid Password"})
-    }   
+    }
     const token = createToken(user._id);
     res.json({success:true,token})
 } catch (error) {
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,24 +1,27 @@
 import express from "express"
 import { addProduct, listProduct , removeProduct} from "../controller/productController.js"
-import multer from "multer"
+// import multer from "multer"
 
 const productRouter = express.Router();
 
 // image storage engine
 
-const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination:"uploads",
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${Date.now()}${file.originalname}`)
+//     }
+// })
 
-const upload = multer({storage:storage})
+// const upload = multer({storage:storage})
 
-productRouter.post("/add",upload.single("image"),addProduct)
+productRouter.post("/add",addProduct)
 productRouter.get("/list",listProduct)
 productRouter.post("/remove",removeProduct)
 
 
 
 export default productRouter;
+
+// with image upload
+// productRouter.post("/add",upload.single("image"),addProduct)
